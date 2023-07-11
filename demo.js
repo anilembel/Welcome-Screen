eff663c24542b0f388941b244f22b2b68a65f7f6
anilembel <anilembelwork@gmail.com>
2023-07-11 16:24:30 +0200

    Welcome page finished

--- demo.js
+++ demo.js
@@ -1,24 +1,23 @@
 async function loadDemo() {
-    await loadDemoFromStrapi()
-    const currentDemoContent = demoContent.data[demoPosition]
-    if (currentDemoContent.attributes.type == "website") {
-        demoContainer = `<iframe id="inlineFrameExample" class="zoom-out" 
+  await loadDemoFromStrapi();
+  const currentDemoContent = demoContent.data[demoPosition];
+  if (currentDemoContent.attributes.type == "website") {
+    demoContainer = `<iframe id="inlineFrameExample" class="zoom-out" 
         title="Inline Frame Example"
         width="2000"
         height="1500"
         src="${currentDemoContent.attributes.appURL}">
-        </iframe>`
-      } 
-      else if (currentDemoContent.attributes.type == "video"){
-        console.log(currentDemoContent.attributes)
-        videoUrl = strapiUrl + currentDemoContent.attributes.video.data.attributes.url
-        demoContainer = `<video src="${videoUrl}" controls autoplay></video>`
-        
-      } else if (currentDemoContent.attributes.type == "app"){
-        pass
-      }
-    console.log(currentDemoContent.attributes.type)
-    return `
+        </iframe>`;
+  } else if (currentDemoContent.attributes.type == "video") {
+    console.log(currentDemoContent.attributes);
+    videoUrl =
+      strapiUrl + currentDemoContent.attributes.video.data.attributes.url;
+    demoContainer = `<video src="${videoUrl}" controls autoplay></video>`;
+  } else if (currentDemoContent.attributes.type == "app") {
+    pass;
+  }
+  console.log(currentDemoContent.attributes.type);
+  return `
     <div class="page-header">
         <button onclick="navigate(\'previous\')">Previous</button>
         <button onclick="navigate(\'home\')">Home</button> 
@@ -28,5 +27,4 @@ async function loadDemo() {
     <div id="demoContainer"> 
     ${demoContainer} 
     </div>`;
-  }
-  
+}
