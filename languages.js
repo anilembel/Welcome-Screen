f8a40432c8a91d2de317784ecb6849830b7ac0b5
anilembel <anilembelwork@gmail.com>
2023-07-11 15:27:45 +0200

    basic settings css 1

--- languages.js
+++ languages.js
@@ -1,6 +1,9 @@
 function loadLanguages() {
-    return `
-      <h2 style="top:200px">Choose your language to continue</h2>
+  return `
+      <header class ="languageHeader">
+        <h2 style="top:200px"> Choose your language to continue</h2>
+      </header>
+      
       <button class="languageButton" onclick="setLanguage('en'); navigate('home')">
         <img src="assets/en.svg" class="languageFlag"></img>
         <p class="languageText">English</p>
@@ -12,5 +15,6 @@ function loadLanguages() {
       <button class="languageButton" onclick="setLanguage('fr-FR'); navigate('home')">
         <img src="assets/fr.svg" class="languageFlag"><p class="languageText">Français</p>
       </button>
-    `;
-  }
+      `;
+}
+s;
